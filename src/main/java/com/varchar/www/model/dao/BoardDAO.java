432ac4a39f99288bc43995f5fa41d5b11a2c1213
Kojae <sle1392@naver.com>
2020-06-02 16:00:08 +0900

    synchroniz master

--- src/main/java/com/varchar/www/model/dao/BoardDAO.java
+++ src/main/java/com/varchar/www/model/dao/BoardDAO.java
@@ -4,10 +4,7 @@ import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
-@Mapper
 public interface BoardDAO {
-	
-	
 	@Select("SELECT eName FROM emp WHERE empNo = #{empNo}")
 	public String getUserName(@Param("empNo") int empNo);
 	
