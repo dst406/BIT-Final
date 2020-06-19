9a4dde407ed8cc7b38db6e103d22200c7bd8ee68
김강산 <ksk6736@naver.com>
2020-06-19 18:44:32 +0900

    0619

--- src/main/java/com/varchar/www/model/domain/manager/SupplierDTO.java
+++ src/main/java/com/varchar/www/model/domain/manager/SupplierDTO.java
@@ -1,8 +1,16 @@
 package com.varchar.www.model.domain.manager;
 
+import java.sql.Date;
+
 import lombok.Data;
 
 @Data
 public class SupplierDTO {
 
+	//구매처
+	private int supplier_no; //구매처 번호
+	private String supplier_name; //구매처명
+	private String supplier_address; //구매처주소
+	private String supplier_tel; //구매처 번호
+	private Date supplier_date; //구매처 등록일
 }
---
+++ src/main/java/com/varchar/www/model/service/AccountingService.java
@@ -0,0 +1,8 @@
+package com.varchar.www.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AccountingService {
+
+}
