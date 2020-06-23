f88c4f0af40c1258bb368b9ad081b544a8207a1f
Kojae <sle1392@naver.com>
2020-06-23 18:27:04 +0900

    board 내비게이션 바  수정중

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,16 +1,29 @@
 $(function(){
 	// 게시판 그룹 불러오기
-	$('.leftbar-tab-menu .nav-link').on('click',function(){
-		$.ajax({
-			url: "/getNavbar/jin2020"
-		}).done(function(data){
+
 			$('.menu-body').html(data);
+			$('.nav-link').removeClass('active');
+			$(event.target).closest('.nav-link').addClass('active');
 			$('.main-icon-menu-pane:first-child .metismenu').addClass('active');
-		//	var popupLink = document.querySelector('.editBoardGroup-popup-link');
 			
-		//	popupLink.addEventListener('click',getModalFormEditPage);
-		})
-		
-	})
+	
+	
+	
+	
+//		$('#board').on('click',function(event){
+//		$.ajax({
+//			url: "/getNavbar/jin2020"
+//		}).done(function(data){
+//			$('.menu-body').html(data);
+//			$('.nav-link').removeClass('active');
+//			$(event.target).closest('.nav-link').addClass('active');
+//			$('.main-icon-menu-pane:first-child .metismenu').addClass('active');
+//			
+//		//	var popupLink = document.querySelector('.editBoardGroup-popup-link');
+//			
+//		//	popupLink.addEventListener('click',getModalFormEditPage);
+//		})
+//		
+//	})
 	
 })
