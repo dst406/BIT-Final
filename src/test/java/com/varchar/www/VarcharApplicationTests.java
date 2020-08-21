40a10951d21c938f91f9e4f9c35cad521dd12142
kojae <sle1392@naver.com>
2020-08-22 05:21:59 +0900

    aws 오류해결용

--- src/test/java/com/varchar/www/VarcharApplicationTests.java
+++ src/test/java/com/varchar/www/VarcharApplicationTests.java
@@ -1,59 +1,59 @@
-package com.varchar.www;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import com.varchar.www.model.dao.BoardDAO;
-import com.varchar.www.model.dao.DAOTest;
-
-import lombok.extern.java.Log;
-
-
-@SpringBootTest
-@Log
-class VarcharApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-	
-	
-	@Autowired
-	private DAOTest daoTest;
-	
-	@Autowired private BoardDAO boardDAO;
-	
-	
-	
-	@Test
-	public void boardGroupListTest() {
-		try {
-		//log.info();
-			System.out.println(boardDAO.getNavbar("jin2020","2").toString());
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	
-	//@Test
-	public void DB_CONNECT_TEST() {
-		System.out.println(daoTest.getSeasonId());
-	}
-	
-	public void boardGroup() {
-		
-		
-		
-	}
-	
-	
-}
-
-
-
-
-
-
-
+//package com.varchar.www;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//import com.varchar.www.model.dao.BoardDAO;
+//import com.varchar.www.model.dao.DAOTest;
+//
+//import lombok.extern.java.Log;
+//
+//
+//@SpringBootTest
+//@Log
+//class VarcharApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//	
+//	
+//	@Autowired
+//	private DAOTest daoTest;
+//	
+//	@Autowired private BoardDAO boardDAO;
+//	
+//	
+//	
+//	@Test
+//	public void boardGroupListTest() {
+//		try {
+//		//log.info();
+//			System.out.println(boardDAO.getNavbar("jin2020","2").toString());
+//		}catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
+//	
+//	
+//	//@Test
+//	public void DB_CONNECT_TEST() {
+//		System.out.println(daoTest.getSeasonId());
+//	}
+//	
+//	public void boardGroup() {
+//		
+//		
+//		
+//	}
+//	
+//	
+//}
+//
+//
+//
+//
+//
+//
+//
